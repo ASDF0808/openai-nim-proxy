f663a592efac068e3deb3cf461d69ab57f3e31f8
Asdf08 <82380582+ASDF0808@users.noreply.github.com>
2026-08-05 21:47:19 +0900

    asdf

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-
+console.log("Using model:", nimModel);
+console.log("Messages:", messages.length);
+console.log("Max tokens:", max_tokens);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
